e4bf0b50c84d38fc6bd15f9346857931c7796fef
seatiel ygnacio rosario <seatiel_2010@hotmail.com>
2017-03-27 17:34:41 -0400

    cambios

---
+++ BLLTests/FacturasBLLTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entidades;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class FacturasBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            //Facturas fac = new Facturas();
+            //Assert.IsTrue(BLL.FacturasBLL.Guardar(new Entidades.Facturas()));
+        }
+
+        [TestMethod()]
+        public void MofidicarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListarTodoTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
